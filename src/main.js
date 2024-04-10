387af02863be6da5a7111220c03f33b87792a32b
“Brenno <dev.brennoeudes@gmail.com>
2024-04-10 17:49:02 -0300

    html and js integrated

---
+++ src/main.js
@@ -0,0 +1,15 @@
+// //AlTERAÇÃO DE ELEMENTOS
+
+// const tituloSessao = document.querySelector('section h3'); // refere-se somente ao h3 dentro da section!
+
+// tituloSessao.setAttribute("id", "titulo-sessao"); // É uma função que espera dois valores, sendo o 1º o atributo a ser definido e o 2º o novo valor!
+// tituloSessao.classList.add("font-grande");
+
+// // Checando as info de classList:
+// // console.log(tituloSessao.classList.contains('font-negrito')); // true ( Ou seja, a font-negrito está dentro da classList de h3!)
+
+// tituloSessao.classList.toggle('font-grande'); // pegando e excluíndo o elemento da classList
+
+// tituloSessao.textContent = 'Título alterado via JavaScript'; // Alterando título via JS!
+// console.log(tituloSessao);
+console.log("Hello, World!");       

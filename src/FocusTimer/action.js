1a81ba407d56b834d3818bcb3e98102ba5d3e4ac
“Brenno <dev.brennoeudes@gmail.com>
2024-04-13 15:44:44 -0300

    actions connections

---
+++ src/FocusTimer/action.js
@@ -0,0 +1,11 @@
+export function toogleRunning() {
+  console.log("ToogleRunning function!");
+}
+
+export function set() {
+  console.log("set!");
+}
+
+export function toogleMusic() {
+    console.log("ToogleMusic function!");
+  }
--- src/FocusTimer/events.js
+++ src/FocusTimer/events.js
@@ -1,25 +1,23 @@
 import { timerControls, musicControls } from "./controls.js";
+import * as actions from "./action.js";
 
 export function registerControls() {
   // EVENTOS
   timerControls.addEventListener("click", (event) => {
     const action = event.target.dataset.action; // target indica o elemento q está sendo clicado // dataset se conecta c/ data no html.
 
-    if (action === undefined) {
+    if (typeof actions[action] !== "function") {
       return;
     }
-
-    console.log(action);
+    actions[action]();
   });
 
   musicControls.addEventListener("click", (event) => {
     const action = event.target.dataset.action; // target indica o elemento q está sendo clicado // dataset se conecta c/ data no html.
 
-    if (action === undefined) {
+    if (typeof actions[action] !== "function") {
       return;
     }
-
-    console.log(action);
+    actions[action]();
   });
-
 }

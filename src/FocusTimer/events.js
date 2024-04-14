2b562bb96d54a7396aba95a734dd5106a642a191
“Brenno <dev.brennoeudes@gmail.com>
2024-04-14 12:51:40 -0300

    events comments

--- src/FocusTimer/events.js
+++ src/FocusTimer/events.js
@@ -1,7 +1,8 @@
 import { timerControls, musicControls } from "./controls.js";
 import * as actions from "./action.js";
 
-  // EVENTOS
+// EVENTOS
+//Captura os eventos e verifica no pacote de ações, se as ações existem/ se fcns são retornadas:
 export function timer() {
   timerControls.addEventListener("click", (event) => {
     const action = event.target.dataset.action; // target indica o elemento q está sendo clicado // dataset se conecta c/ data no html.

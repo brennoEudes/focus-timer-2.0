16ae939a0da37bd19f609d045e41cdda8ecc5f95
“Brenno <dev.brennoeudes@gmail.com>
2024-04-14 17:58:40 -0300

    updateDisplay config

--- src/FocusTimer/controls.js
+++ src/FocusTimer/controls.js
@@ -1,2 +1,5 @@
 export const timerControls = document.querySelector(".timer-controls");
 export const musicControls = document.querySelector(".music-controls");
+
+export const minutes = document.querySelector("#minutes");
+export const seconds = document.querySelector("#seconds");

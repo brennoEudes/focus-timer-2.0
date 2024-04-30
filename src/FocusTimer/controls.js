a10f74f7cca2a28435e670352a938544cf6baafe
“Brenno <dev.brennoeudes@gmail.com>
2024-04-30 15:19:51 -0300

    const btns defined

--- src/FocusTimer/controls.js
+++ src/FocusTimer/controls.js
@@ -3,5 +3,10 @@ export const musicControls = document.querySelector(".music-controls");
 export const addBtn = document.getElementById('add-btn');
 export const removeBtn = document.getElementById('remove-btn');
 
+export const forestButton = document.querySelector("#forest-sound-btn");
+export const rainButton = document.querySelector("#rain-sound-btn");
+export const coffeeButton = document.querySelector("#coffee-sound-btn");
+export const fireplaceButton = document.querySelector("#fireplace-sound-btn");
+
 export const minutes = document.querySelector("#minutes");
 export const seconds = document.querySelector("#seconds");
